e878de300e4cfbe64ca419a3560b53e4253e8bb1
dzodzooo <stojanovicjovana312@gmail.com>
2026-01-06 21:18:32 +0100

    feat(#1): categories update

--- public/script.js
+++ public/script.js
@@ -1,9 +1,21 @@
-function deleteCategory(category_id) {
-  fetch("http://host.docker.internal:3000/categories/" + category_id, {
-    method: "DELETE",
-  })
-    .then(alert("delete"))
-    .catch((reason) => {
-      alert(reason);
-    });
+function updateCategory(category) {
+  let categoryModalBg = document.getElementById("categoryModalBg");
+  categoryModalBg.classList.remove("none");
+  categoryModalBg.classList.add("modalBg");
+  let categoryModal = document.getElementById("categoryModal");
+  categoryModal.removeAttribute("hidden");
+  categoryModal.classList.add("modal");
+  let nameinput = document.getElementById("nameCatPut");
+  nameinput.value = category.name;
+  let idinput = document.getElementById("idCatPut");
+  idinput.value = category.id;
+}
+
+function closeModal() {
+  let categoryModal = document.getElementById("categoryModal");
+  categoryModal.classList.remove("modal");
+  categoryModal.setAttribute("hidden", "");
+  let categoryModalBg = document.getElementById("categoryModalBg");
+  categoryModalBg.classList.remove("modalBg");
+  categoryModalBg.classList.add("none");
 }
